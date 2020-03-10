85a954ba2f47e92dc8493849c956d1bd51af348e
Johnny Jing <jingpeng@cn.ibm.com>
2020-03-10 14:20:16 +0800

    fix shell install script

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -5,6 +5,7 @@ cd ~/.vim_runtime
 
 echo 'set runtimepath+=~/.vim_runtime
 
+source ~/.vim_runtime/vimrcs/vim-plug-list.vim
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
