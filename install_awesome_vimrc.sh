7c5655c7274ecd9acbc7d99f7f3115d1717975e1
johnny <jingpeng@cn.ibm.com>
2020-10-08 20:34:40 +0800

    update vim installer for Both VIM and NVIM.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -16,4 +16,4 @@ source ~/.vim_runtime/my_configs.vim
 catch
 endtry' > ~/.vimrc
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+echo "Installed the Ultimate Vim configuration into Vim successfully! Enjoy :-)"
