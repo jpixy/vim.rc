a315d700e689ac44fd1099bfff39f7452e8978fc
Johnny Jing <jingpeng@cn.ibm.com>
2020-10-17 14:39:15 +0800

    add new install.sh and modify readme

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 git clone --depth=1 https://github.com/johnnyjpixy/vim-conf.git ~/.vim_runtime
 
 # Setup VIM env
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+# sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # Setup NeoVIM env
-sh ~/.vim_runtime/install_awesome_neovimrc.sh
+# sh ~/.vim_runtime/install_awesome_neovimrc.sh
