19ac78703f81deb6dff88926a320571547738ea1
Johnny <13565121+johnnyjpixy@users.noreply.github.com>
2020-10-27 16:05:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-git clone --depth=1 https://github.com/johnnyjpixy/vim-conf.git ~/.vim_runtime
+git clone --depth=1 https://github.com/johnnyjpixy/vim.rc.git ~/.vim_runtime
 
 # Setup VIM env
 # sh ~/.vim_runtime/install_awesome_vimrc.sh
